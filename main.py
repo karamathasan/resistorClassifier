6565be99cf5623b7b6bee600605c6a6a799f1ae4
karamathasan <karamathasan420@gmail.com>
2024-10-03 23:47:41 -0400

    fixed csv data loading into categorical data. Beginning data fitting

--- main.py
+++ main.py
@@ -20,59 +20,65 @@ img_height = int(3024/16)
 img_width = int(4032/16)
 num_classes = 4
 
-# model = Sequential([
-#   layers.Rescaling(1./255, input_shape=(img_height, img_width, 3)),
-#   layers.Conv2D(16, 3, padding='same', activation='relu'),
-#   layers.MaxPooling2D(),
-#   layers.Conv2D(32, 3, padding='same', activation='relu'),
-#   layers.MaxPooling2D(),
-#   layers.Dropout(0.2),
-#   layers.Conv2D(64, 3, padding='same', activation='relu'),
-#   layers.MaxPooling2D(),
-#   layers.Dropout(0.4),
-#   layers.Flatten(),
-#   layers.Dense(128, activation='relu'),
-#   layers.Dense(num_classes)
-# ])
-# model.compile(\
-#     optimizer='adam',
-#     loss = keras.losses.CategoricalCrossentropy(from_logits=True),
-#     metrics=[keras.metrics.CategoricalAccuracy()]
-#     )
+model = Sequential([
+  layers.Rescaling(1./255, input_shape=(img_height, img_width, 3)),
+  layers.Conv2D(16, 3, padding='same', activation='relu'),
+  layers.MaxPooling2D(),
+  layers.Conv2D(32, 3, padding='same', activation='relu'),
+  layers.MaxPooling2D(),
+  layers.Dropout(0.2),
+  layers.Conv2D(64, 3, padding='same', activation='relu'),
+  layers.MaxPooling2D(),
+  layers.Dropout(0.4),
+  layers.Flatten(),
+  layers.Dense(128, activation='relu'),
+  layers.Dense(num_classes)
+])
+model.compile(\
+    optimizer='adam',
+    loss = keras.losses.CategoricalCrossentropy(from_logits=True),
+    metrics=[keras.metrics.CategoricalAccuracy()]
+    )
 
 dir = "Data3/train/"
 data = pd.read_csv("Data3/train/train.csv")
+
 files = data['Image_Path']
-labels = pd.get_dummies(data, columns=["Class"], dtype=int)
-labels = labels.drop("Image_Path", axis=1)
+labels = pd.get_dummies(data.astype(str), prefix="resistor_", columns=["Class"], dtype=int)
+labels = labels.drop(["ID", "Image_Path"], axis=1)
 
 path = pathlib.Path(dir).with_suffix('')
-img_count = len(list(path.glob("*/*,jpg")))
-print(img_count)
+# img_count = len(list(path.glob("*/*.png")))
 
-# X_train, X_test, y_train, y_test = train_test_split(files, labels, shuffle = True)
+X_train, X_test, y_train, y_test = train_test_split(files, labels, shuffle = True)
 
-# epochs = 10
-# batch_size = 64
-print(data.head())
+epochs = 10
+batch_size = 64
+
+print(labels.head())
 
 # /////// ITERATIVE FITTING ///////
-# for j in range(epochs):
-#     print(f"    epoch {j+1}/{epochs}")
-#     X_train, X_test, y_train, y_test = train_test_split(files, labels, shuffle = True)
-#     X = []
-#     y = []
-#     for i in range(len(X_train)):
-#         if (i % batch_size == 0 or i == len(X_train)-1) and i != 0: 
-#             X = np.array(X)
-#             y = np.array(y)
-#             model.fit(X,y, batch_size=batch_size)
-#             X = []
-#             y = []
-#         img = PIL.Image.open(dir + X_train.iloc[i])
-#         img = keras.utils.img_to_array(img)
-#         X.append(img)
-#         y.append(y_train.iloc[i])
+for j in range(epochs):
+    print(f"epoch {j+1}/{epochs}")
+    X_train, X_test, y_train, y_test = train_test_split(files, labels, shuffle = True)
+    X = []
+    y = []
+    for i in range(len(X_train)):
+        print(f"     iteration: {i}")
+        if (i % batch_size == 0 or i == len(X_train)-1) and i != 0: 
+            try:
+                X = np.array(X)
+            except:
+                raise Exception(f"X = np.array(X) failed. X: {X}")
+            y = np.array(y)
+            model.fit(X,y, batch_size=batch_size)
+            X = []
+            y = []
+        img = PIL.Image.open(dir + X_train.iloc[i])
+        img.resize((img_width,img_height))
+        img = keras.utils.img_to_array(img)
+        X.append(img)
+        y.append(y_train.iloc[i])
 
 # //////// TESTING /////////
 # score = 0
